a8f2ab1aaa54d2813f7ab5147de8ca2f16a82f55
moganesan <developer.moganesan@gmail.com>
2023-12-14 22:55:22 +0530

    font added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,20 @@ import type { AppProps } from "next/app";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Web5ContextProvider } from "@/context/web5";
+import { VT323 } from "next/font/google";
+
+const font = VT323({ subsets: ["latin"], weight: "400" });
+
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Web5ContextProvider>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen />
+        <main className={font.className}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen />
+        </main>
       </Web5ContextProvider>
     </QueryClientProvider>
   );
