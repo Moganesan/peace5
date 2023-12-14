20350991c3ef4978c9e04ce800214a4916b369f6
moganesan <developer.moganesan@gmail.com>
2023-12-14 23:10:37 +0530

    add button added in passwords page

--- pages/passwords.tsx
+++ pages/passwords.tsx
@@ -1,9 +1,15 @@
 import Layout from "@/components/layout";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 const Passwords = () => {
   return (
     <Layout>
-      <h1 className="text-3xl">Passwords</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl">Passwords</h1>
+        <button className="border-2 p-4 text-xl">
+          <PlusIcon className="w-5 h-5" />
+        </button>
+      </div>
       <div className="grid grid-cols-5 mt-10 grid-flow-row">
         <div className="border-2 w-64 p-5 cursor-pointer hover:scale-105 duration-500">
           <img src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" />
