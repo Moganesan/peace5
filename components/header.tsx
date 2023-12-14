70c674cb4a7a6d3844cdf4b144f3e0deb7689335
moganesan <developer.moganesan@gmail.com>
2023-12-14 22:54:21 +0530

    header component styles updated

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,15 @@
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { useEffect } from "react";
+import { useWeb5Contex } from "@/context/web5";
+
 const Header = () => {
+  const { did } = useWeb5Contex();
+
   return (
-    <nav className="absolute bg-black z-10 w-full h-20">
+    <nav className="absolute bg-black z-10 w-full px-10 flex flex-col justify-center h-20">
       <div className="flex items-center justify-end">
-        <h1>ht.moganesan@gmail.com</h1>
+        <UserCircleIcon className="w-10 h-10" />
+        <h1>Moganesan</h1>
       </div>
     </nav>
   );
