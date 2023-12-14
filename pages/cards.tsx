3d2e483c3742378ca9aadcd09404c2876e9c100c
moganesan <developer.moganesan@gmail.com>
2023-12-15 00:20:16 +0530

    cards page added

--- pages/cards.tsx
+++ pages/cards.tsx
@@ -2,7 +2,13 @@ import Layout from "@/components/layout";
 const Cards = () => {
   return (
     <Layout>
-      <h1>Cards</h1>
+      <h1 className="text-3xl">Cards</h1>
+      <div className="grid grid-cols-5 mt-10 grid-flow-row gap-20">
+        <div className="credit-card visa selectable">
+          <div className="credit-card-last4">4242</div>
+          <div className="credit-card-expiry">08/25</div>
+        </div>
+      </div>
     </Layout>
   );
 };
